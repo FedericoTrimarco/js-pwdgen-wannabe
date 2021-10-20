8bf375c526fbced32a87bca93d14dfa88ce72bd3
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-10-20 13:56:56 +0200

    add name e surname

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ PASSWORD GENERATOR
 */
 
 // NOME UTENTE
-
+const nameUser = prompt('Qual è il tuo nome?')
+console.log(nameUser, typeof nameUser)
 // CONGNOME UTENTE
-
+const surnameUser = prompt('Qual è il tuo cognome?')
+console.log(surnameUser, typeof surnameUser)
 // COLORE PREFERITO
 
 // RISULTATO
