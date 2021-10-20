ebd6fbf186bd2962973e6ff31cb3db557a7b64c3
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-10-20 14:08:00 +0200

    canc all console.log

--- js/main.js
+++ js/main.js
@@ -4,19 +4,15 @@ PASSWORD GENERATOR
 
 // NOME UTENTE
 const nameUser = prompt('Qual è il tuo nome?');
-console.log(nameUser, typeof nameUser);
 
 // CONGNOME UTENTE
 const surnameUser = prompt('Qual è il tuo cognome?');
-console.log(surnameUser, typeof surnameUser);
 
 // COLORE PREFERITO
 const color = prompt('Qual è il tuo colore preferito?');
-console.log(color, typeof color);
 
 // PASSWORD
 const password = nameUser + surnameUser + color;
-console.log(password);
 
 // RISULTATO A SCHERMO
 document.getElementById('result').innerHTML = `${password}23`;
