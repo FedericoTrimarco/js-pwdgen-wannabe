25b032a7c73fd15a454027b45962b623ffc07806
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-10-20 14:05:36 +0200

    add color, password and result

--- js/main.js
+++ js/main.js
@@ -3,11 +3,20 @@ PASSWORD GENERATOR
 */
 
 // NOME UTENTE
-const nameUser = prompt('Qual è il tuo nome?')
-console.log(nameUser, typeof nameUser)
-// CONGNOME UTENTE
-const surnameUser = prompt('Qual è il tuo cognome?')
-console.log(surnameUser, typeof surnameUser)
-// COLORE PREFERITO
+const nameUser = prompt('Qual è il tuo nome?');
+console.log(nameUser, typeof nameUser);
 
-// RISULTATO
+// CONGNOME UTENTE
+const surnameUser = prompt('Qual è il tuo cognome?');
+console.log(surnameUser, typeof surnameUser);
+
+// COLORE PREFERITO
+const color = prompt('Qual è il tuo colore preferito?');
+console.log(color, typeof color);
+
+// PASSWORD
+const password = nameUser + surnameUser + color;
+console.log(password);
+
+// RISULTATO A SCHERMO
+document.getElementById('result').innerHTML = `${password}23`;
